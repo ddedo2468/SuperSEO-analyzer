8518315360373a34c4a220e664190a109489f87f
Abdullah <abdallahmosbah25@gmail.com>
2024-10-06 14:40:09 +0300

    ci: added swagger docs to the API updated the requierments.txt file

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+from rest_framework.permissions import AllowAny
 
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title='Seo Analyzer API',
+        default_version='1.0.0',
+        description='API Documentation for the Super Seo Analyzer',
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/docs', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema'),
     path("api/keywords/", include("keywords_extractor.urls")),
     path("api/", include("urls.urls")),
     path("users/", include("users.urls")),
